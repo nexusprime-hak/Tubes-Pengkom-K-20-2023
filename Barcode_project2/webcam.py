9f135dc4649d05486cfbd29319970bb9c9af1e8f
MGabriel45 <mjibrahim45@gmail.com>
2023-10-18 19:00:31 +0700

    komen sudah cukup

--- Barcode_project2/webcam.py
+++ Barcode_project2/webcam.py
@@ -20,14 +20,14 @@
 # app : WebcamApp = sebuah class untuk mengambil video dari kamera laptop
 
 # import modul yang dibutuhkan
-import cv2
-import tkinter as tk
-import time
-import datetime as dt
-import pandas as pd
-import beepy
-from pyzbar.pyzbar import decode
-from PIL import Image, ImageTk
+import cv2                          # modul ini untuk menangkap kamera video dari laptop
+import tkinter as tk                # modul ini untuk membuat gui
+import time                         # modul ini untuk menghentikan sejenak jalan program
+import datetime as dt               # modul ini untuk mendapatkan waktu
+import pandas as pd                 # modul ini untuk megatur dan mengolah data dari excel dan untuk membuat excel
+import beepy                        # modul ini untuk membuat suara
+from pyzbar.pyzbar import decode    # modul ini untuk mendecode qr code menjadi text
+from PIL import Image, ImageTk      # modul ini untuk membuat gui menampilkan video kamera yang telah ditangkap
 
 # variabel global
 kelasPRDexcel_path = "Barcode_project2/kelasPRD.xlsx"    # relative path untuk kelasPRD.xlsx
@@ -42,18 +42,19 @@ class WebcamApp:                                     # class untuk menangkap vid
         # window : tkinter = berupa gui untuk menampilkan video kamera
         # window_title : string = nama dari gui yang akan ditampilkan
 
-        self.window = window
-        self.window.title(window_title)
-        self.window.iconbitmap(barcode_ico_path)
+        self.window = window                        # kita masukkan video kamera ke gui
+        self.window.title(window_title)             # kita buat judul gui sesuai dengan window_title
+        self.window.iconbitmap(barcode_ico_path)    # kita akan membuat ikon gui sesuai dengan ikon yang kita mau
 
-        self.vid = cv2.VideoCapture(0)
+        self.vid = cv2.VideoCapture(0)              # kita gunakan fungsi dari modul cv2 untuk mengambil video kamera laptop
 
-        self.canvas = tk.Canvas(window, width=self.vid.get(3), height=self.vid.get(4))
-        self.canvas.pack()
+        self.canvas = tk.Canvas(window, width=self.vid.get(3), height=self.vid.get(4))    # kita mengatur besar gui yang ditampilkan saat menjalankan aplikasi
+        self.canvas.pack()                          # kita tampilkan gui ke layar
 
-        self.update()
-        self.window.mainloop()
+        self.update()                     # panggil fungsi update()
+        self.window.mainloop()            # fungsi ini agar aplikasi berjalan terus selama tidak di close
 
+    # fungsi untuk mengupdate gui agar gui yang ditampilkan sesuai dengan kamera laptop
     def update(self):
         ret, frame = self.vid.read()
 
@@ -62,18 +63,17 @@ class WebcamApp:                                     # class untuk menangkap vid
             self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
 
         for i in decode(frame):
-            value = i.data.decode('utf-8')
-            nim_code = value[:3]
-            time.sleep(0.5)
+            value = i.data.decode('utf-8')      # kita decode qr code menjadi text utf-8
+            time.sleep(0.5)                     # kita hentikan program sejenak agar tidak terus menerus mendecode qr code
 
             output = True
-            try:
+            try:                                # tes apakah data dari qr code berupa sebuah angka
                 angka = int(value)
             except:
                 output = False
 
-            if len(value) == 16 and output:
-                if int(value[:8]) in df_kehadiran.index:
+            if len(value) == 16 and output:                   # cek apakah data yg didapatkan sesuai yang diharapkan
+                if int(value[:8]) in df_kehadiran.index:      # cek apakah nim termasuk pada list di excel
                     df_kehadiran['Status'][int(value[:8])] = "Hadir"
                     df_kehadiran['Waktu Absensi'][int(value[:8])] = dt.datetime.now().strftime("%H:%M:%S")
                     beepy.beep(sound=5)
@@ -81,7 +81,7 @@ class WebcamApp:                                     # class untuk menangkap vid
 
         self.window.after(1, self.update)
 
-# Create a window and pass it to the WebcamApp class
+# membuat window gui lalu dimasukkan ke WebcamApp
 
 root = tk.Tk()
 app = WebcamApp(root, "QR Scanner")
